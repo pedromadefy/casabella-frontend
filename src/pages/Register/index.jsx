4b89767f6e4cf85e593c847c47614e0f0b5a772a
Pedro Ricardo Oliveira Córdova <pedroricardocordova@yahoo.com.br>
2022-11-30 13:27:50 -0300

    Fea: Register form step 1 e 2

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import Stepper from "../../components/stepper/Stepper";
+
 import * as S from "./styles.js";
 import logoOrange from "../../assets/imgs/logo-casabella-fragrancias-laranja-login.png";
 import Input from "../../components/input/Input";
-import Button from '../../components/button/Button'
+import Button from "../../components/button/Button";
+
+import { ToastContainer, toast } from "react-toastify";
 
 export default function Register() {
   const [name, setName] = useState("");
@@ -11,66 +13,352 @@ export default function Register() {
   const [date, setDate] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+  const [step, setStep] = useState(1);
+
+  const [readyStepTwo, setReadyStepTwo] = useState(false);
+  const [readyStepThree, setReadyStepThree] = useState(false);
+  const [readyStepFour, setReadyStepFour] = useState(false);
+
+  function handleValidateStepTwo() {
+    if (name !== "") {
+      setReadyStepTwo(true);
+      setStep(2);
+    } else {
+      toast("Insira todos dados");
+    }
+  }
+
+  function handleValidateStepThree() {
+    setReadyStepTwo(true);
+  }
+  function handleValidateStepFour() {
+    setReadyStepThree(true);
+  }
+
+  function handleStepTwo() {
+    if (readyStepTwo) {
+      setStep(2)
+    }
+  }
+  function handleStepThree() {
+    if (readyStepThree) {
+    }
+  }
+  function handleStepFour() {
+    if (readyStepFour) {
+    }
+  }
+
   return (
-    <S.Container>
-      <img src={logoOrange} alt="logo" />
+    <>
+      {step === 1 ? (
+        <S.Container>
+          <img src={logoOrange} alt="logo" />
+          <S.SliderContainer>
+            <div>
+              <S.RangeSliderStepOne>
+                <div></div>
+              </S.RangeSliderStepOne>
 
-      <Stepper />
-      <S.WelcomeMessage>
-        <h1>Bem vindo, antes de tudo...</h1>
-        <p>Você pode alterar tudo depois.</p>
-      </S.WelcomeMessage>
+              <S.SliderStep>
+                <button onClick={() => setStep(1)}>1</button>
+                <button onClick={handleStepTwo}>2</button>
+                <button onClick={handleStepThree}>3</button>
+                <button onClick={handleStepFour}>4</button>
+              </S.SliderStep>
+            </div>
+          </S.SliderContainer>
+          <S.WelcomeMessage>
+            <h1>Bem vindo, antes de tudo...</h1>
+            <p>Você pode alterar tudo depois.</p>
+          </S.WelcomeMessage>
 
-      <div>
-        <form action="">
-          <Input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Nome"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <S.DateRow className="date-row">
-            <Input
-              type="text"
-              name="cpf"
-              id="cpf"
-              placeholder="Nome"
-              value={cpf}
-              onChange={(e) => setCpf(e.target.value)}
-            />
-            <Input
-              type="text"
-              name="date"
-              id="date"
-              placeholder="Data de Nascimento"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-            />
-          </S.DateRow>
-          <Input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="E-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            type="text"
-            name="date"
-            id="date"
-            placeholder="Telefone"
-            value={phone}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </form>
+          <div>
+            <form action="">
+              <Input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Nome completo"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <S.DateRow className="date-row">
+                <Input
+                  type="text"
+                  name="cpf"
+                  id="cpf"
+                  placeholder="CPF"
+                  value={cpf}
+                  onChange={(e) => setCpf(e.target.value)}
+                />
+                <Input
+                  type="text"
+                  name="date"
+                  id="date"
+                  placeholder="Data de Nascimento"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                />
+              </S.DateRow>
+              <Input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="text"
+                name="date"
+                id="date"
+                placeholder="Telefone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </form>
 
-        <Button>Continuar</Button>
-      </div>
+            <Button
+              onClick={() => {
+                handleValidateStepTwo();
+              }}
+            >
+              Continuar
+            </Button>
+          </div>
+          <S.CancelContent>Cancelar e voltar</S.CancelContent>
+        </S.Container>
+      ) : null}
 
-      
-    </S.Container>
+      {step === 2 ? (
+        <S.Container>
+          <img src={logoOrange} alt="logo" />
+          <S.SliderContainer>
+            <div>
+              <S.RangeSliderStepTwo>
+                <div></div>
+              </S.RangeSliderStepTwo>
+
+              <S.SliderStep>
+                <button onClick={() => setStep(1)}>1</button>
+                <button onClick={handleStepTwo}>2</button>
+                <button onClick={handleStepThree}>3</button>
+                <button onClick={handleStepFour}>4</button>
+              </S.SliderStep>
+            </div>
+          </S.SliderContainer>
+          <S.WelcomeMessage>
+            <h1>{name}, nos envie seu RG ou CNH...</h1>
+            <p>Não se preocupe, é seguro e privado.</p>
+          </S.WelcomeMessage>
+
+          <div>
+            <form>
+              <Input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Nome completo"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <S.DateRow className="date-row">
+                <Input
+                  type="text"
+                  name="cpf"
+                  id="cpf"
+                  placeholder="CPF"
+                  value={cpf}
+                  onChange={(e) => setCpf(e.target.value)}
+                />
+                <Input
+                  type="text"
+                  name="date"
+                  id="date"
+                  placeholder="Data de Nascimento"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                />
+              </S.DateRow>
+              <Input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="text"
+                name="date"
+                id="date"
+                placeholder="Telefone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </form>
+
+            <Button
+              onClick={() => {
+                handleValidateStepThree();
+              }}
+            >
+              Continuar
+            </Button>
+          </div>
+          <S.CancelContent>Cancelar e voltar</S.CancelContent>
+        </S.Container>
+      ) : null}
+
+      {step === 3 ? (
+        <S.Container>
+          <img src={logoOrange} alt="logo" />
+          <S.SliderContainer>
+            <div>
+              <S.RangeSliderStepThree>
+                <div></div>
+              </S.RangeSliderStepThree>
+
+              <S.SliderStep>
+                <button onClick={() => setStep(1)}>1</button>
+                <button onClick={handleStepTwo}>2</button>
+                <button onClick={handleStepThree}>3</button>
+                <button onClick={handleStepFour}>4</button>
+              </S.SliderStep>
+            </div>
+          </S.SliderContainer>
+          <S.WelcomeMessage>
+            <h1>Bem vindo, antes de tudo...</h1>
+            <p>Você pode alterar tudo depois.</p>
+          </S.WelcomeMessage>
+
+          <div>
+            <form action="">
+              <Input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Nome completo"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <S.DateRow className="date-row">
+                <Input
+                  type="text"
+                  name="cpf"
+                  id="cpf"
+                  placeholder="CPF"
+                  value={cpf}
+                  onChange={(e) => setCpf(e.target.value)}
+                />
+                <Input
+                  type="text"
+                  name="date"
+                  id="date"
+                  placeholder="Data de Nascimento"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                />
+              </S.DateRow>
+              <Input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="text"
+                name="date"
+                id="date"
+                placeholder="Telefone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </form>
+
+            <Button onClick={handleValidateStepFour()}>Continuar</Button>
+          </div>
+          <S.CancelContent>Cancelar e voltar</S.CancelContent>
+        </S.Container>
+      ) : null}
+
+      {step === 4 ? (
+        <S.Container>
+          <img src={logoOrange} alt="logo" />
+          <S.SliderContainer>
+            <div>
+              <S.RangeSliderStepFour>
+                <div></div>
+              </S.RangeSliderStepFour>
+
+              <S.SliderStep>
+                <button onClick={() => setStep(1)}>1</button>
+                <button onClick={handleStepTwo}>2</button>
+                <button onClick={handleStepThree}>3</button>
+                <button onClick={handleStepFour}>4</button>
+              </S.SliderStep>
+            </div>
+          </S.SliderContainer>
+          <S.WelcomeMessage>
+            <h1>Bem vindo, antes de tudo...</h1>
+            <p>Você pode alterar tudo depois.</p>
+          </S.WelcomeMessage>
+
+          <div>
+            <form action="">
+              <Input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Nome completo"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <S.DateRow className="date-row">
+                <Input
+                  type="text"
+                  name="cpf"
+                  id="cpf"
+                  placeholder="CPF"
+                  value={cpf}
+                  onChange={(e) => setCpf(e.target.value)}
+                />
+                <Input
+                  type="text"
+                  name="date"
+                  id="date"
+                  placeholder="Data de Nascimento"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                />
+              </S.DateRow>
+              <Input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="text"
+                name="date"
+                id="date"
+                placeholder="Telefone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </form>
+
+            <Button>Continuar</Button>
+          </div>
+          <S.CancelContent>Cancelar e voltar</S.CancelContent>
+        </S.Container>
+      ) : null}
+      <ToastContainer />
+    </>
   );
 }
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -17,15 +17,135 @@ export const Container = styled.div`
 
 export const DateRow = styled.div`
   display: flex;
+  width: 100%;
   justify-content: space-between;
+  input {
+    width: 100%;
+    margin-right: 0.5rem;
+  }
 `;
 
 export const WelcomeMessage = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-text-align: center;
-margin-bottom: 1.875rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin-bottom: 1.875rem;
+`;
+export const CancelContent = styled.div`
+  margin-top: 2rem;
+`;
+export const RangeSliderStepOne = styled.div`
+  width: 100%;
+  display: flex;
+  margin-top: 4rem;
+  margin-bottom: -4.2rem;
+  justify-content: center;
+  align-items: center;
+  -webkit-appearance: none; /* Override default CSS styles */
+  appearance: none;
 
-`
+  outline: none; /* Remove outline */
+  ::before {
+    content: "";
+    background:linear-gradient(90deg, rgba(230,78,51,1) 20%, rgba(208,209,213,1) 20%);
+  ;
+    width: 80%;
+    height: 2px;
+
+  
+  }
+`;
+export const RangeSliderStepTwo = styled.div`
+  width: 100%;
+  display: flex;
+  margin-top: 4rem;
+  margin-bottom: -4.2rem;
+  justify-content: center;
+  align-items: center;
+  -webkit-appearance: none; /* Override default CSS styles */
+  appearance: none;
+
+  outline: none; /* Remove outline */
+  ::before {
+    content: "";
+    background:linear-gradient(90deg, rgba(230,78,51,1) 50%, rgba(208,209,213,1) 50%);
+  ;
+    width: 80%;
+    height: 2px;
+
+  
+  }
+`;
+export const RangeSliderStepThree = styled.div`
+  width: 100%;
+  display: flex;
+  margin-top: 4rem;
+  margin-bottom: -4.2rem;
+  justify-content: center;
+  align-items: center;
+  -webkit-appearance: none; /* Override default CSS styles */
+  appearance: none;
+
+  outline: none; /* Remove outline */
+  ::before {
+    content: "";
+    background:linear-gradient(90deg, rgba(230,78,51,1) 80%, rgba(208,209,213,1) 80%);
+  ;
+    width: 80%;
+    height: 2px;
+
+  
+  }
+`;
+export const RangeSliderStepFour = styled.div`
+  width: 100%;
+  display: flex;
+  margin-top: 4rem;
+  margin-bottom: -4.2rem;
+  justify-content: center;
+  align-items: center;
+  -webkit-appearance: none; /* Override default CSS styles */
+  appearance: none;
+
+  outline: none; /* Remove outline */
+  ::before {
+    content: "";
+    background:linear-gradient(90deg, rgba(230,78,51,1) 100%, rgba(208,209,213,1) 0%);
+  ;
+    width: 80%;
+    height: 2px;
+
+  
+  }
+`;
+
+export const SliderContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+export const SliderStep = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 1rem;
+    margin: 2.6875rem;
+    color: #fff;
+    border: none;
+    width: 2.625rem;
+    height: 2.625rem;
+    border-radius: 50%;
+    background-color: #e64e33;
+  }
+`;
